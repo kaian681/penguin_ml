b6c7ce9f2e9dd8b5b52b495aa9be469e972c734c
kaian681 <kaian.ammann@wingd.com>
2026-07-06 16:31:02 +0200

    adjustment

--- penguins_ml.py
+++ penguins_ml.py
@@ -14,6 +14,10 @@ st.write("This app uses 6 inputs to predict the species of penguin using"
          "a modl built on the Palmer's Penguin's dataset. use the from below"
          "to get started!")
 
+password_guess = st.text_input("What is the Password?")
+if password_guess != "streamlit_is_great":
+    st.stop()
+
 penguin_df = pd.read_csv("penguins.csv")
 penguin_df.dropna(inplace=True)
 output = penguin_df["species"]
